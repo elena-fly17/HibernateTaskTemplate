d5580e07e8062450081c34f2175825aeaad95ce9
elena-fly17 <elena17mr@gmail.com>
2022-01-13 12:45:35 +0200

    Версия коннектора обновлена - с 5.1.38 до 8.0.27

--- src/main/java/jm/task/core/jdbc/Main.java
+++ src/main/java/jm/task/core/jdbc/Main.java
@@ -1,7 +1,11 @@
 package jm.task.core.jdbc;
 
+import jm.task.core.jdbc.util.Util;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello");
+        Util util = new Util();
+        util.dbOpenConnection();
+        util.dbCloseConnection();
     }
 }
--- src/main/java/jm/task/core/jdbc/util/Util.java
+++ src/main/java/jm/task/core/jdbc/util/Util.java
@@ -1,5 +1,29 @@
 package jm.task.core.jdbc.util;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
 public class Util {
     // реализуйте настройку соеденения с БД
+    private static final String URL = "jdbc:mysql://localhost:3306/mydatabase1";
+    private static final String USERNAME = "root17";
+    private static final String PASSWORD = "root17";
+    Connection connection;
+
+    public void dbOpenConnection () {
+        try {
+            connection = DriverManager.getConnection(URL, USERNAME, PASSWORD);
+        } catch (SQLException e) {
+            System.out.println("Не удалось загрузить драйвер");
+        }
+    }
+
+    public void dbCloseConnection () {
+        try {
+            connection.close();
+        } catch (SQLException e) {
+            System.out.println("Не удалось закрыть соединение");
+        }
+    }
 }
